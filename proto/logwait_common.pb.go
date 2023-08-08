770ed2f572a9e5895ff5bd5b206b5c38436ccbbb
Jille Timmermans <jille@snoozethis.com>
2023-08-08 22:45:03 +0200

    proto: Extend the protocol to allow the scanners to reject an observable

--- proto/logwait_common.pb.go
+++ proto/logwait_common.pb.go
@@ -138,6 +138,61 @@ func (x *Observable) GetFilters() []*Filter {
 	return nil
 }
 
+type RejectObservable struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id      string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Message string `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *RejectObservable) Reset() {
+	*x = RejectObservable{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_logwait_common_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *RejectObservable) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RejectObservable) ProtoMessage() {}
+
+func (x *RejectObservable) ProtoReflect() protoreflect.Message {
+	mi := &file_logwait_common_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RejectObservable.ProtoReflect.Descriptor instead.
+func (*RejectObservable) Descriptor() ([]byte, []int) {
+	return file_logwait_common_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *RejectObservable) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *RejectObservable) GetMessage() string {
+	if x != nil {
+		return x.Message
+	}
+	return ""
+}
+
 var File_logwait_common_proto protoreflect.FileDescriptor
 
 var file_logwait_common_proto_rawDesc = []byte{
@@ -154,10 +209,14 @@ var file_logwait_common_proto_rawDesc = []byte{
 	0x72, 0x65, 0x12, 0x38, 0x0a, 0x07, 0x66, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x73, 0x18, 0x03, 0x20,
 	0x03, 0x28, 0x0b, 0x32, 0x1e, 0x2e, 0x63, 0x6f, 0x6d, 0x2e, 0x73, 0x6e, 0x6f, 0x6f, 0x7a, 0x65,
 	0x74, 0x68, 0x69, 0x73, 0x2e, 0x6c, 0x6f, 0x67, 0x77, 0x61, 0x69, 0x74, 0x2e, 0x46, 0x69, 0x6c,
-	0x74, 0x65, 0x72, 0x52, 0x07, 0x66, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x73, 0x42, 0x23, 0x5a, 0x21,
-	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x53, 0x6e, 0x6f, 0x6f, 0x7a,
-	0x65, 0x54, 0x68, 0x69, 0x73, 0x2d, 0x6f, 0x72, 0x67, 0x2f, 0x6c, 0x6f, 0x67, 0x77, 0x61, 0x69,
-	0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x65, 0x72, 0x52, 0x07, 0x66, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x73, 0x22, 0x3c, 0x0a, 0x10,
+	0x52, 0x65, 0x6a, 0x65, 0x63, 0x74, 0x4f, 0x62, 0x73, 0x65, 0x72, 0x76, 0x61, 0x62, 0x6c, 0x65,
+	0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64,
+	0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x23, 0x5a, 0x21, 0x67, 0x69,
+	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x53, 0x6e, 0x6f, 0x6f, 0x7a, 0x65, 0x54,
+	0x68, 0x69, 0x73, 0x2d, 0x6f, 0x72, 0x67, 0x2f, 0x6c, 0x6f, 0x67, 0x77, 0x61, 0x69, 0x74, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -172,10 +231,11 @@ func file_logwait_common_proto_rawDescGZIP() []byte {
 	return file_logwait_common_proto_rawDescData
 }
 
-var file_logwait_common_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_logwait_common_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_logwait_common_proto_goTypes = []interface{}{
-	(*Filter)(nil),     // 0: com.snoozethis.logwait.Filter
-	(*Observable)(nil), // 1: com.snoozethis.logwait.Observable
+	(*Filter)(nil),           // 0: com.snoozethis.logwait.Filter
+	(*Observable)(nil),       // 1: com.snoozethis.logwait.Observable
+	(*RejectObservable)(nil), // 2: com.snoozethis.logwait.RejectObservable
 }
 var file_logwait_common_proto_depIdxs = []int32{
 	0, // 0: com.snoozethis.logwait.Observable.filters:type_name -> com.snoozethis.logwait.Filter
@@ -216,6 +276,18 @@ func file_logwait_common_proto_init() {
 				return nil
 			}
 		}
+		file_logwait_common_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RejectObservable); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -223,7 +295,7 @@ func file_logwait_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_logwait_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
