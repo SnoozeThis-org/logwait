d45b22df8c0046186e9936b94291fa5199db13eb
Jille Timmermans <jille@snoozethis.com>
2023-09-19 21:28:35 +0200

    Untested version of a Loki scanner

--- go.mod
+++ go.mod
@@ -7,20 +7,25 @@ require (
 	github.com/Jille/easymutex v1.1.0
 	github.com/Jille/genericz v0.4.0
 	github.com/fsnotify/fsnotify v1.6.0
+	github.com/grafana/loki/pkg/push v0.0.0-20230919104151-a8d5815510bd
 	github.com/influxdata/go-syslog/v3 v3.0.0
+	github.com/klauspost/compress v1.17.0
 	github.com/spf13/pflag v1.0.5
-	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/stretchr/testify v1.8.3 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 )
+
+replace github.com/grafana/loki/pkg/push v0.0.0-20230919104151-a8d5815510bd => github.com/Jille/loki/pkg/push v0.0.0-20230919191926-b7b103c7c7f3
---
+++ scanners/loki/loki-scanner.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/SnoozeThis-org/logwait/config"
+	"github.com/SnoozeThis-org/logwait/scanners/common"
+	gogo "github.com/gogo/protobuf/proto"
+	"github.com/grafana/loki/pkg/push"
+	"github.com/klauspost/compress/s2"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 0, "Port to open for HTTP")
+
+	srv     *common.Service
+	learner *common.FieldLearner
+)
+
+func main() {
+	config.Parse()
+
+	c, err := grpc.Dial(*common.ObserverAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Cannot connect to observer at %q: %v", *common.ObserverAddress, err)
+	}
+
+	srv = common.NewService(c, "loki-scanner")
+	go srv.ConnectLoop()
+	learner = srv.CreateFieldLearner('.')
+	learner.Seen("line")
+	srv.WaitForInitialConnection()
+
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/loki/api/v1/push" {
+		http.NotFound(w, r)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	body, err = s2.Decode(nil, body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	var msg push.PushRequest
+	if err := gogo.Unmarshal(body, &msg); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	for _, s := range msg.Streams {
+		for _, e := range s.Entries {
+			handleMessage(e.Line, e.StructuredMetadata)
+		}
+	}
+
+	w.WriteHeader(204)
+}
+
+func handleMessage(line string, labels push.LabelsAdapter) {
+	for _, l := range labels {
+		learner.Seen(l.Name)
+	}
+	srv.MatchObservables(func(o common.Observable) bool {
+	loop:
+		for field, regexp := range o.Regexps {
+			switch field {
+			case "line":
+				if !regexp.MatchString(line) {
+					return false
+				}
+			default:
+				for _, l := range labels {
+					if l.Name == field {
+						if !regexp.MatchString(l.Value) {
+							return false
+						}
+						continue loop
+					}
+				}
+				return false
+			}
+		}
+		return true
+	})
+}
